e81a41dc2231a2e06187d96e371c4d85d434fb4b
vinsunkavalli <vinsunkavalli@gmail.com>
2020-08-07 13:46:52 -0700

    cleaned up redirect and made it work

--- redirect.js
+++ redirect.js
@@ -1,11 +1,16 @@
 //redirecting tab urls to a different url
 
-bad_url = "https://www.reddit.com"
-good_url = "https://www.google.com"
+bad_url = "https://www.reddit.com/"
+good_url = "https://www.google.com/"
 
-chrome.tabs.onCreated.addListener(function (tab){
-    if(tab.url == bad_url){
+var tabRedirect = function(tab){
+    if(tab.url.localeCompare(bad_url) == 0){
         chrome.tabs.update(tab.id, {url: good_url});
+        console.log("yo");
+    }else{
+        console.log(tab.url);
     }
-});
-//chrome.tabs.onUpdated.addListener();
+}
+
+chrome.tabs.onCreated.addListener(function(tab){tabRedirect(tab)});
+chrome.tabs.onUpdated.addListener(function(tabID, newTabInfo, tab){tabRedirect(tab)});
