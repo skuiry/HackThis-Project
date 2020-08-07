f9caba8b381d62ca2654758f411cef728532f1a5
Abhig2002 <34994192+Abhig2002@users.noreply.github.com>
2020-08-07 14:13:14 -0700

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,4 +1,18 @@
 
+var fs = require('fs');
   const filePath = "C:\Users\abhig\HackThis-Project\Blacklist.txt";
   let websites = [];
-  
+  var prompt = window.prompt("How many websites would you blacklist?:" );
+
+  for(int i = 0; i < prompt; i++){
+    websites.push(window.prompt("Enter website #" + i));
+  }
+
+  for ( int i = 0; i < websites.length; i++){
+      fs.writeFile(filePath, websites[i], function (err) {
+          if (err)  {
+            throw err;
+          }
+          console.log('Replaced!');
+      });
+  }
