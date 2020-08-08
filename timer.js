7bac603fe7fdacc11fbfaa95968d95976ead75d2
skuiry <57824978+shnackquery@users.noreply.github.com>
2020-08-08 15:59:47 -0700

    timer fixed, 1 sec increment + manifest change

---
+++ timer.js
@@ -0,0 +1,38 @@
+var i = 0;
+var tab;
+bad_url = "https://www.reddit.com/";
+var time = setInterval(timer, 1000);
+
+function timer(){
+  chrome.tabs.query({url : bad_url}, function (tabs){
+
+    if (tabs.length != 0){
+      if (i > 0){
+        i--;
+        console.log(i);
+      }
+      else{
+        stopDistraction();
+      }
+    }
+    else{
+      i++;
+      console.log(i);
+    }
+  });
+chrome.tabs.query({}, function (tabs){
+    if (tabs.length == 0){
+      console.log("hi");
+      clearInterval(time);
+    }
+  });
+}
+
+function stopDistraction(){
+chrome.tabs.query({url : bad_url}, function (tabs) {
+	for (var x = 0; x < tabs.length; x++){
+		chrome.tabs.remove(parseInt(tabs[x].id));
+		console.log("tab has been sent to hell");
+	}
+	});
+}
