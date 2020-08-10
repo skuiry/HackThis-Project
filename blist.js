6145fd5ce28f3f169b2b387ce550762bb072e750
vinsunkavalli <vinsunkavalli@gmail.com>
2020-08-10 16:21:53 -0700

    varuna modified settings.html

--- blist.js
+++ blist.js
@@ -1,14 +1,28 @@
 const fs = require("fs");
+const blocklistJSON = require("./blocklist.json");
 
-const blacklisted = require("./blacklist");
+var blocklist = blocklistJSON.blocklist;
 
-let badURL = {
-    url: "https://www.twitter.com/"
-};
+function addBlock(url){
+  blocklist.push(url);
+  blocklistJSON.blocklist = blocklist;
+}
 
-blacklisted.push(badURL);
+function removeBlock(url){
+  var index = blocklist.indexOf(url);
+  if(index != -1){
+    blocklist.splice(index, 1);
+  }
+}
 
-fs.writeFile("blacklist.json", JSON.stringify(blacklisted), err => {
-  if (err) throw err;
-  console.log("Successfully wrote to blacklist.");
-});
+function containsBlock(url){
+  var index = blocklist.indexOf(url);
+  return (index == -1);
+}
+
+function write(){
+  fs.writeFile("blocklist.json", JSON.stringify(blocklistJSON), err => {
+    if (err) throw err;
+    console.log("Successfully wrote to blocklist.");
+  });
+}
